31c448f6e18cfc7361f572be58e7d19393023ed8
kattan12345 <renoir5d@gmail.com>
2022-05-05 22:48:11 +0900

    Try Apple's tutorial

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by 勝田啓一 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by 勝田啓一 on 2022/05/05.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
